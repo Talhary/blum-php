090fe21c01a27b5e0c70b58e59055704c7b801c4
Talhary <talhariaz5425869@gmail.com>
2024-10-24 19:56:23 +0500

    fdsfds

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/project-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7182caa52b7626da7f52868559272bce6fca654d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7182caa52b7626da7f52868559272bce6fca654d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
